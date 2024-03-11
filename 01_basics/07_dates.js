c6cb5bc45e4c9eb6e5d726e999f05644944ba699
Kushagra Kapoor <kushagrakapoor27@gmail.com>
2024-03-11 21:18:52 +0530

    Learnt about, numbers, math lib, date and arrays.

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,16 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toJSON());
+
+console.log(typeof myDate);
+
+let myCreatedDate = new Date(2024, 0, 23)
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeroes = ["shaktiman", "krishh"]
+
+console.log(myArr[0]);
+
+
+// Array Methods
+
+myArr.push(6)
+console.log(myArr);
+
+myArr.pop()
+console.log(myArr);
